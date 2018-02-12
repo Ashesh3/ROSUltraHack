91e1f4bb20ad16fb53acfafddc9e76181b6ed20c
Yoga <LastRoze@users.noreply.github.com>
2018-02-13 02:45:05 +0800

    Update Resources.cs
    Optimize Code Readability

--- Properties/Resources.cs
+++ Properties/Resources.cs
@@ -1,9 +1,3 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: RosPublicCheat.Properties.Resources
-// Assembly: ReliefExternal, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 03EB7061-76EF-46FD-89DF-12D90B29AAE1
-// Assembly location: C:\Users\Zero-Sum\Desktop\Dumps\ReliefExternal-cleaned.exe
-
 using System.CodeDom.Compiler;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -13,40 +7,40 @@ using System.Runtime.CompilerServices;
 
 namespace Whynot.Properties
 {
-  [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
-  [CompilerGenerated]
-  [DebuggerNonUserCode]
-  internal class Resources
-  {
-    private static ResourceManager resourceMan;
-    private static CultureInfo resourceCulture;
-
-    [EditorBrowsable(EditorBrowsableState.Advanced)]
-    internal static ResourceManager ResourceManager
+    [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [CompilerGenerated]
+    [DebuggerNonUserCode]
+    internal class Resources
     {
-      get
-      {
-        if (Whynot.Properties.Resources.resourceMan == null)
-          Whynot.Properties.Resources.resourceMan = new ResourceManager("RosPublicCheat.Properties.Resources", typeof (Whynot.Properties.Resources).Assembly);
-        return Whynot.Properties.Resources.resourceMan;
-      }
-    }
+        private static ResourceManager resourceMan;
+        private static CultureInfo resourceCulture;
 
-    [EditorBrowsable(EditorBrowsableState.Advanced)]
-    internal static CultureInfo Culture
-    {
-      get
-      {
-        return Whynot.Properties.Resources.resourceCulture;
-      }
-      set
-      {
-        Whynot.Properties.Resources.resourceCulture = value;
-      }
-    }
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static ResourceManager ResourceManager
+        {
+            get
+            {
+                if (Whynot.Properties.Resources.resourceMan == null)
+                Whynot.Properties.Resources.resourceMan = new ResourceManager("RosPublicCheat.Properties.Resources", typeof (Whynot.Properties.Resources).Assembly);
+                return Whynot.Properties.Resources.resourceMan;
+            }
+        }
 
-    internal Resources()
-    {
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static CultureInfo Culture
+        {
+            get
+            {
+                return Whynot.Properties.Resources.resourceCulture;
+            }
+            set
+            {
+                Whynot.Properties.Resources.resourceCulture = value;
+            }
+        }
+
+        internal Resources()
+        {
+        }
     }
-  }
 }
