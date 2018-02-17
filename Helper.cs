b0cfe21594726defce652d28f0af1997a6c2db3c
Yoga <LastRoze@users.noreply.github.com>
2018-02-17 12:24:13 +0800

    Fixed Crash/Lag issues
    Optimize Readability

--- Helper.cs
+++ Helper.cs
@@ -1,17 +1,16 @@
-﻿ 
 using System;
 
 namespace Whynot
 {
-  internal class Helper
-  {
-    public static int GetDistance(Vector3 v1, Vector3 v2, int Divide)
+    internal class Helper
     {
-      Vector3 vector3 = new Vector3();
-      vector3.X = v1.X - v2.X;
-      vector3.Y = v1.Y - v2.Y;
-      vector3.Z = v1.Z - v2.Z;
-      return (int) Math.Sqrt(Math.Pow((double) vector3.X, 2.0) + Math.Pow((double) vector3.Y, 2.0) + Math.Pow((double) vector3.Z, 2.0)) / Divide;
+        public static int GetDistance(Vector3 v1, Vector3 v2, int Divide)
+        {
+            Vector3 vector3 = new Vector3();
+            vector3.X = v1.X - v2.X;
+            vector3.Y = v1.Y - v2.Y;
+            vector3.Z = v1.Z - v2.Z;
+            return (int) Math.Sqrt(Math.Pow((double) vector3.X, 2.0) + Math.Pow((double) vector3.Y, 2.0) + Math.Pow((double) vector3.Z, 2.0)) / Divide;
+        }
     }
-  }
 }
