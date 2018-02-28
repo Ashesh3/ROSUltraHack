00eb7286618041d70952cb05eacab3424a345571
Hax0r <3626859+Ashesh3@users.noreply.github.com>
2018-02-28 17:40:34 +0530

    Update Offsets.cs

--- Offsets.cs
+++ Offsets.cs
@@ -2,12 +2,12 @@ namespace Whynot
 {
     internal class Offsets
     {
-        public static int Client = 0x1EDE6B8;
-        public static int LocalPlayer = 0x1EDEA74;
+        public static int Client = 0x1EF8AF8;
+        public static int LocalPlayer = 0x1EFA7F4;
         public static int m_ppObjects = 3740;
         public static int nickname = 380;
-        public static int Render = 0x1E82B28;
-        public static int SceneContext = 0x1CDC4D0;
-        public static int ViewMatrix = 0x1CDC4D0;
+        public static int Render = 0x1E9D2B0;
+        public static int SceneContext = 0x1CF6E18;
+        public static int ViewMatrix = 0x1CF6E18;
     }
 }
