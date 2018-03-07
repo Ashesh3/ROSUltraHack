85c952b923f59211c1b25113d8da05672ae4fc98
Hax0r <3626859+Ashesh3@users.noreply.github.com>
2018-03-07 14:57:58 +0530

    Updated for 7/3/18

--- Offsets.cs
+++ Offsets.cs
@@ -1,13 +1,13 @@
-﻿namespace Whynot
+namespace Whynot
 {
     internal class Offsets
     {
-        public static int Client = 0x1EF8AF8;
-        public static int LocalPlayer = 0x1EFA7F4;
+        public static int Client = 0x20E63D8;
+        public static int LocalPlayer = 0x20E80D4;
         public static int m_ppObjects = 3740;
         public static int nickname = 380;
-        public static int ViewMatrix = 0x1CF6E18;
-        public static int Grass = 0x01C3F19C;
+        public static int ViewMatrix = 0x1ED92A0;
+        public static int Grass = 0x01C3F19C; //old offset
         public static int Passthrough = 0x157CA48; // old offset
     }
 }
