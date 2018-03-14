a772d6d679b33707f4eab49e82882dc8baa3565b
Hax0r <3626859+Ashesh3@users.noreply.github.com>
2018-03-14 14:32:56 +0530

    Updated for 14/03/18 update

--- Offsets.cs
+++ Offsets.cs
@@ -2,11 +2,11 @@
 {
     internal class Offsets
     {
-        public static int Client = 0x20E63D8;
-        public static int LocalPlayer = 0x20E80D4;
+        public static int Client = 0x20E8590;
+        public static int LocalPlayer = 0x20EA28C;
         public static int m_ppObjects = 3740;
         public static int nickname = 380;
-        public static int ViewMatrix = 0x1ED92A0;
+        public static int ViewMatrix = 0x1EDB470;
         public static int Grass = 0x01C3F19C; //old offset
         public static int Passthrough = 0x157CA48; // old offset
     }
